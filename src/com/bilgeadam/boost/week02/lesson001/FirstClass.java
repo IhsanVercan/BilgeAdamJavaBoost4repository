3edff3e4acb2b641692ce683d54fbf6f0a193e37
IhsanVercan <ihsanvercan@hotmail.com>
2022-08-15 15:33:30 +0300

    first commit

---
+++ src/com/bilgeadam/boost/week02/lesson001/FirstClass.java
@@ -0,0 +1,11 @@
+package com.bilgeadam.boost.week02.lesson001;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+
+		// this is the first java class of the boost4 course
+	
+	}
+
+}
